543a9d07c06c513473ac4870d0bce46223176b13
Mare Trojan <99330513+MarekTrojanOSSP@users.noreply.github.com>
2025-02-25 11:45:56 +0100

    Add files via upload

---
+++ Pain1/main.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/registr.html')
+def registr():
+    return render_template('registr.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
